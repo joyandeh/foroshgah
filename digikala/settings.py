7ce39e513486bdaf0fc5631a12072b9986b9c2d5
Erfan <erfan.joyandeh@gmail.com>
2025-08-03 23:49:09 +0330

    sevom

--- digikala/settings.py
+++ digikala/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-+eq5o2l&^v5hd-#2go$_&1)338v=yutt$hrgda1kfr+$w=5(0v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mojasemeh.ir']
 
 
 # Application definition
@@ -110,6 +110,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATICFILES_URLS = ['static/']
 STATICFILES_DIRS = [ BASE_DIR / "static",]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
